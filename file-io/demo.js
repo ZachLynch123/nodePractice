361f99ff94dd4a953126cff62234dabcd1bdfd76
HankBedford <Zachary.Lynch1337@gmail.com>
2019-02-05 14:53:30 -0800

    Accessing json from file system

--- file-io/demo.js
+++ file-io/demo.js
@@ -1,2 +1,11 @@
 const fs = require('fs');
 
+const data = require('./data.json');
+console.log(data.name);
+
+
+fs.readFile('./data.json', 'utf-8' ,(err, data) => {
+    let ah = JSON.parse(data)
+    console.log(ah.name);
+    
+});
