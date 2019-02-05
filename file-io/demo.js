3533e30f04f40d2a7c24c7f052497b758b9f4e96
HankBedford <Zachary.Lynch1337@gmail.com>
2019-02-05 14:54:54 -0800

    Accessing files

--- file-io/demo.js
+++ file-io/demo.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 
-const data = require('./data.json');
+/* const data = require('./data.json');
 console.log(data.name);
-
+ 
 
 fs.readFile('./data.json', 'utf-8' ,(err, data) => {
     let ah = JSON.parse(data)
     console.log(ah.name);
+    
+}); */
+
+fs.readdir('c:/', (err, data) => {
+    console.log(data);
     
 });
