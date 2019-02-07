1a873ac7a593d4acbf2db11f9ff82d5b6d8c6cec
Zach <Zachary.Lynch1337@gmail.com>
2019-02-07 10:27:54 -0800

    Created get request to obtain static posts from server

--- chatApp/server.js
+++ chatApp/server.js
@@ -6,6 +6,17 @@ const port = process.env.PORT || 5000;
 
 // Statically serving data
 app.use(express.static(__dirname));
+
+let messages = [
+    {name: "Tim", message: "hello!"},
+    {name: "Alli", message: "hello world!!"},
+]
+
+// Get request
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
 const server = app.listen(port, () => console.log(`server started on port ${port}`, server.address().port)
 );
 
