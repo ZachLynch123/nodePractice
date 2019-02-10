9ff2e2817987187c099a6f0af75a5bb3407c716a
Zach <Zachary.Lynch1337@gmail.com>
2019-02-10 08:05:43 -0800

    async tests with jasmine

--- chatApp/spec/server.spec.js
+++ chatApp/spec/server.spec.js
@@ -1,5 +1,16 @@
-describe('calc', () => {
-    it('should multiply 2 and 2', () => {
-        expect(2*2).toBe(5);
+const request = require('request');
+
+describe('get messages', () => {
+    it('should return 200 Ok', (done) => {
+        request.get('http://localhost:5000/messages', (e, res) => {
+            expect(res.statusCode).toEqual(200);
+            done();
+        });
+    });
+    it('should return list that is not empty', (done) => {
+        request.get('http://localhost:5000/messages', (e, res) => {
+            expect(JSON.parse(res.body).length).toBeGreaterThan(0);
+            done();
+        });
     });
 });
